37cadfc12c9586f42c376668a09519d97e929111
remy29 <arthur.d.remy@gmail.com>
2020-11-04 22:20:53 +0000

    added conn.write and linted

--- client.js
+++ client.js
@@ -4,17 +4,20 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => { // Error Message for idling
-    console.log("you ded cuz you idled *don't boot the snoot*")
+  conn.setEncoding('utf8');
+  conn.on('data', () => { // Error Message for idling
+    console.log("you ded cuz you idled *don't boot the snoot*");
   });
   conn.on('connect', () => { // Welcome message on connect
-    console.log("welcome to snek, may your day be snekful")
+    console.log("welcome to snek, may your day be snekful");
+  });
+  conn.on('connect', () => { // Welcome message on connect
+    conn.write("Name: MCH");
   });
   return conn;
 };
