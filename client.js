b6cf8ca96d8c61df47e8d1f951a08be6d7e66ddf
remy29 <arthur.d.remy@gmail.com>
2020-11-04 22:13:30 +0000

    added connection module

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => { // Error Message for idling
+    console.log("you ded cuz you idled *don't boot the snoot*")
+  });
+  conn.on('connect', () => { // Welcome message on connect
+    console.log("welcome to snek, may your day be snekful")
+  });
+  return conn;
+};
+
+module.exports = { connect };
