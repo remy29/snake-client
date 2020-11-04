c4c1a9601bf5651f1c4e1a52408898448f96326a
remy29 <arthur.d.remy@gmail.com>
2020-11-04 23:39:09 +0000

    added WASD movement

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 const { stdin } = require('process');
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.data = handleUserInputs;
   stdin.setRawMode(true);
@@ -14,6 +17,18 @@ const handleUserInputs = function() {
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    }
+    else if (key === 'w') {
+      connection.write("Move: up")
+    }
+    else if (key === 'a') {
+      connection.write("Move: left")
+    }
+    else if (key === 's') {
+      connection.write("Move: down")
+    }
+    else if (key === 'd') {
+      connection.write("Move: right")
     }
   });
 };
--- play.js
+++ play.js
@@ -3,8 +3,7 @@ const {setupInput} = require('./input');
 
 
 console.log('Connecting ...');
-connect();
 
-setupInput();
+setupInput(connect());
 
 
