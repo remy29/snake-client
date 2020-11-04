f8845177090ac96c55dec578ee1a53dae4ec9bb9
remy29 <arthur.d.remy@gmail.com>
2020-11-04 22:36:18 +0000

    learned about using setInterval to automatically submit commands)

--- client.js
+++ client.js
@@ -13,12 +13,15 @@ const connect = function() {
   conn.on('data', () => { // Error Message for idling
     console.log("you ded cuz you idled *don't boot the snoot*");
   });
-  conn.on('connect', () => { // Welcome message on connect
+  /* conn.on('connect', () => { // Welcome message on connect
     console.log("welcome to snek, may your day be snekful");
-  });
-  conn.on('connect', () => { // Welcome message on connect
+  }); */
+  conn.on('connect', () => { // says name on connect
+    console.log("welcome to snek, may your day be snekful");
     conn.write("Name: MCH");
+    //setInterval(() => conn.write("Move: up"), 50);
   });
+  
   return conn;
 };
 
